ca9a572b24bddc56485801b8c130b7bae750cc9e
Julian Villa <julian.villa@metricamovil.com>
2024-09-22 15:09:04 -0600

    subiendo varios proyectos

---
+++ motor_control/motor_control.ino
@@ -0,0 +1,12 @@
+int motorAPin1 = 5; // Pin 5 de L293 
+int motorBPin2 = 6;// Pin 6 de L293
+
+void setup() {
+  pinMode (motorAPin1, OUTPUT);
+  pinMode (motorBPin2, OUTPUT);
+}
+void loop() {
+  digitalWrite (motorAPin1, HIGH); // Al activar gira el motor A en sentido de las agujas del digitalWrite(motorBPin2, HIGH); // Al activar gira el motor B en sentido de las agujas del
+  digitalWrite (motorBPin2, HIGH);
+ // while(1){} // Esta parte del codigo inicia un bucle inifinito.
+} 
---
+++ sketch_sep04a/sketch_sep04a.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+int LedR = 2;
+float temperatura;
+void setup()
+{
+  pinMode(LedR, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int valorArduino = analogRead(A0);
+  float voltaje = (valorArduino/1024.0)*5.0;
+  temperatura = (voltaje - 0.5)*100;
+  Serial.print (" ValorArduino:");
+  Serial.print(valorArduino);
+  Serial.print(" Voltaje:");
+  Serial.print (voltaje);
+  Serial.print (" Temperatura:");
+  Serial.println (temperatura);
+}
