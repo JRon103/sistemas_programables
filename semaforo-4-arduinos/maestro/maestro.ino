beb74c82a838ab78fc4d4415ab92f1561247aaae
Julian Villa <julian.villa@metricamovil.com>
2024-11-15 16:58:14 -0600

    delay en maestros

--- semaforo-4-arduinos/maestro/maestro.ino
+++ semaforo-4-arduinos/maestro/maestro.ino
@@ -19,7 +19,7 @@ void loop() {
   // Rutina: Izquierdas de Juárez (encienden rojo en ID 1 y 2)
   sendCommand(SEMAFORO_JUAREZ_DERECHA, 3); // Enciende LED rojo
   sendCommand(SEMAFORO_JUAREZ_IZQUIERDA, 3); // Enciende LED rojo
-  delay(2000); // Mantiene encendido durante 5 segundos
+  delay(6000); // Mantiene encendido durante 5 segundos
 
   // Apaga todos los LEDs antes de la siguiente rutina
   apagarTodos();
@@ -27,7 +27,7 @@ void loop() {
   // Rutina: Recto de Juárez (encienden verde en ID 1 y 2)
   sendCommand(SEMAFORO_JUAREZ_DERECHA, 2); // Enciende LED verde
   sendCommand(SEMAFORO_JUAREZ_IZQUIERDA, 2); // Enciende LED verde
-  delay(2000); // Mantiene encendido durante 5 segundos
+  delay(6000); // Mantiene encendido durante 5 segundos
 
   // Apaga todos los LEDs antes de la siguiente rutina
   apagarTodos();
@@ -35,7 +35,7 @@ void loop() {
   // Rutina: Recto de Xochi (encienden rojo en ID 3 y 4)
   sendCommand(SEMAFORO_XOCHI_ARRIBA, 2); // Enciende LED rojo
   sendCommand(SEMAFORO_XOCHI_ABAJO, 2); // Enciende LED rojo
-  delay(2000); // Mantiene encendido durante 5 segundos
+  delay(6000); // Mantiene encendido durante 5 segundos
 }
 
 // Función para apagar todos los LEDs en los esclavos
@@ -51,7 +51,7 @@ void sendCommand(int slaveAddress, int command) {
  
    Serial.print ("Comando: ");
   Serial.println(command);
-   Serial.print("Arduin: ");
+   Serial.print("Arduino: ");
    Serial.println(slaveAddress);
   
   Wire.beginTransmission(slaveAddress);
