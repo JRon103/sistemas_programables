8f7a435b0afd5b3f9e74d843da5275cf98fe4c4e
Julian Villa <julian.villa@metricamovil.com>
2024-11-15 16:56:09 -0600

    vuelta

--- semaforo-4-arduinos/esclavo1/esclavo1.ino
+++ semaforo-4-arduinos/esclavo1/esclavo1.ino
@@ -5,12 +5,22 @@
 #define GREEN_PIN 4
 #define BLUE_PIN 5
 
+#define RED_PIN_VUELTA 10
+#define GREEN_PIN_VUELTA 11
+#define BLUE_PIN_VUELTA 12
+
+
 void setup() {
   Wire.begin(8); // Cambia para cada esclavo (8, 9, 10, 11)
   Wire.onReceive(receiveEvent); 
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
+   
+  pinMode(RED_PIN_VUELTA, OUTPUT);
+  pinMode(GREEN_PIN_VUELTA, OUTPUT);
+  pinMode(BLUE_PIN_VUELTA, OUTPUT);
+  
   apagarRGB(); // Apaga el LED RGB al inicio
   Serial.begin(9600);
 }
@@ -24,15 +34,24 @@ void apagarRGB() {
   digitalWrite(RED_PIN, LOW);
   digitalWrite(GREEN_PIN, LOW);
   digitalWrite(BLUE_PIN, LOW);
+
+  digitalWrite(RED_PIN_VUELTA, LOW);
+  digitalWrite(GREEN_PIN_VUELTA, LOW);
+  digitalWrite(BLUE_PIN_VUELTA, LOW);
 }
 
 // Rutina para encender el color deseado
+void encenderColorVuelta(int red, int green, int blue) {
+  digitalWrite(RED_PIN_VUELTA, red);
+  digitalWrite(GREEN_PIN_VUELTA, green);
+  digitalWrite(BLUE_PIN_VUELTA, blue);
+}
+
 void encenderColor(int red, int green, int blue) {
-  digitalWrite(RED_PIN, red);
+  digitalWrite(RED_PIN, red);q
   digitalWrite(GREEN_PIN, green);
   digitalWrite(BLUE_PIN, blue);
 }
-
 void receiveEvent(int bytes) {
   int command = Wire.read(); // Lee el comando
   Serial.print("Comando recibido: ");
@@ -55,20 +74,20 @@ void receiveEvent(int bytes) {
 
     encenderColor(HIGH, LOW, LOW); // Rojo
   } else if (command == 3) { // Procedimiento para vuelta
-    encenderColor(LOW, HIGH, LOW); // Verde sólido
+    encenderColorVuelta(LOW, HIGH, LOW); // Verde sólido
     delay(2000);
 
     for (int i = 0; i < 4; i++) { // Verde parpadeando
-      encenderColor(LOW, HIGH, LOW);
+      encenderColorVuelta(LOW, HIGH, LOW);
       delay(250);
       apagarRGB();
       delay(250);
     }
 
-    encenderColor(HIGH, HIGH, LOW); // Amarillo
+    encenderColorVuelta(HIGH, HIGH, LOW); // Amarillo
     delay(2000);
 
-    encenderColor(HIGH, LOW, LOW); // Rojo
+    encenderColorVuelta(HIGH, LOW, LOW); // Rojo
   } else {
     apagarRGB();
   }
