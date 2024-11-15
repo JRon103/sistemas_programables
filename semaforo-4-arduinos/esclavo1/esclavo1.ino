454224f775d33c63c39172ef36d5b9613c71a9f8
Julian Villa <julian.villa@metricamovil.com>
2024-11-15 13:26:13 -0600

    se cambia el codigo de esclavo para que ahora siga la rutina de un semaforo y utilice un led rgb de 4 terminales

--- semaforo-4-arduinos/esclavo1/esclavo1.ino
+++ semaforo-4-arduinos/esclavo1/esclavo1.ino
@@ -1,35 +1,75 @@
 #include <Wire.h>
-#define GREEN_PIN 51  // LED verde
-#define RED_PIN 3    // LED rojo
+
+// Pines del LED RGB
+#define RED_PIN 3
+#define GREEN_PIN 4
+#define BLUE_PIN 5
 
 void setup() {
-  Wire.begin(8); // Cambiar para cada esclavo (8, 9, 10, 11)
+  Wire.begin(8); // Cambia para cada esclavo (8, 9, 10, 11)
   Wire.onReceive(receiveEvent); 
-  pinMode(GREEN_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  apagarRGB(); // Apaga el LED RGB al inicio
   Serial.begin(9600);
-  //digitalWrite(GREEN_PIN, HIGH);
-  
 }
 
 void loop() {
-
   // El loop queda vacío
+}
+
+// Función para apagar el LED RGB
+void apagarRGB() {
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(BLUE_PIN, LOW);
+}
+
+// Rutina para encender el color deseado
+void encenderColor(int red, int green, int blue) {
+  digitalWrite(RED_PIN, red);
+  digitalWrite(GREEN_PIN, green);
+  digitalWrite(BLUE_PIN, blue);
 }
 
 void receiveEvent(int bytes) {
   int command = Wire.read(); // Lee el comando
-  Serial.println("comando: ");
+  Serial.print("Comando recibido: ");
   Serial.println(command);
-  // Apaga ambos LEDs antes de encender el correspondiente
-  digitalWrite(GREEN_PIN, LOW);
-  digitalWrite(RED_PIN, LOW);
 
-  // Controla los LEDs en función del comando recibido
-  if (command == 2) {
-    digitalWrite(GREEN_PIN, HIGH);  // Enciende LED verde
-    Serial.println("verde prendido");
-  } else if (command == 3) {
-    digitalWrite(RED_PIN, HIGH);    // Enciende LED rojo
+  // Rutinas para los comandos
+  if (command == 2) { // Procedimiento para verde
+    encenderColor(LOW, HIGH, LOW); // Verde sólido
+    delay(2000);
+
+    for (int i = 0; i < 4; i++) { // Verde parpadeando
+      encenderColor(LOW, HIGH, LOW);
+      delay(250);
+      apagarRGB();
+      delay(250);
+    }
+
+    encenderColor(HIGH, HIGH, LOW); // Amarillo
+    delay(2000);
+
+    encenderColor(HIGH, LOW, LOW); // Rojo
+  } else if (command == 3) { // Procedimiento para vuelta
+    encenderColor(LOW, HIGH, LOW); // Verde sólido
+    delay(2000);
+
+    for (int i = 0; i < 4; i++) { // Verde parpadeando
+      encenderColor(LOW, HIGH, LOW);
+      delay(250);
+      apagarRGB();
+      delay(250);
+    }
+
+    encenderColor(HIGH, HIGH, LOW); // Amarillo
+    delay(2000);
+
+    encenderColor(HIGH, LOW, LOW); // Rojo
+  } else {
+    apagarRGB();
   }
 }
