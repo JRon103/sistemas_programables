daf0b27ff29260b2c6ff5c173d70209e32d1e048
Gerardo Martinez <gerartmartinez@gmail.com>
2024-11-24 18:49:39 -0600

    feat(wip): adds data visualization on index page
    Functionality still not finished

---
+++ frontend-wygan/frontend-wygan/src/components/DataTable.tsx
@@ -0,0 +1,53 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Box, Paper } from '@mui/material';
+
+const mockDataRows = [
+    { id: 1, cardNumber: 'FJREIOFJ3' },
+    { id: 2, cardNumber: 'ABCD1234' },
+    { id: 3, cardNumber: 'XYZ7890' },
+];
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 100, headerAlign: 'center', align: 'center' },
+    { field: 'cardNumber', headerName: 'Card Number', width: 200, headerAlign: 'center', align: 'center' },
+    {
+        field: 'updateOption',
+        headerName: '',
+        width: 100,
+        align: 'center',
+        sortable: false,
+        renderCell: (params) => (<a>Actualizar</a>)
+    },
+    {
+        field: 'deleteOption',
+        headerName: '',
+        width: 100,
+        align: 'center',
+        sortable: false,
+        renderCell: (params) => (<a>Eliminar</a>)
+    }
+];
+
+export default function DataTable() {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Paper sx={{ height: 400, width: 'auto' }}>
+                <DataGrid
+                    rows={mockDataRows}
+                    columns={columns}
+                    pageSizeOptions={[5, 10]}
+                    paginationModel={{ page: 0, pageSize: 10 }}
+                    disableRowSelectionOnClick={true}
+                />
+            </Paper>
+
+        </Box>
+
+    );
+}
