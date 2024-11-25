7a417db02867549a3f75992b6499453dc0a217b8
Gerardo Martinez <gerartmartinez@gmail.com>
2024-11-24 20:37:34 -0600

    feat(wip): adds CreateButton component

---
+++ frontend-wygan/src/components/CreateButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+export default function CreateButton() {
+    return (
+        <Button variant="contained" sx={{ textTransform: 'none' }}>
+            Agregar
+        </Button>
+    )
+}
